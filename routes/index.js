9bc469dcd850a12524754c318e5a2e6ec2e7f0bc
long.phan <dragons3232@gmail.com>
2023-12-19 14:23:37 +0700

    Create test endpoint to insert record

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
+const db = require('../src/kysely/database').db;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/test', async function (req, res, next) {
+  const record = await db.insertInto('SmsCode')
+    .values({
+      phone: '123456789',
+      code: '1234',
+    })
+    .executeTakeFirst();
+
+  res.json({ success: record });
 });
 
 module.exports = router;
